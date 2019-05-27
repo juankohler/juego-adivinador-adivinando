f0f6a0cd3e67345614da98d833bf67bef2ebf694
juan kohler <juancho14.jk@gmail.com>
2019-05-27 20:33:12 -0300

    terminado pruebas unitarias de juego 2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 import * as Game1 from '../js/game1'
-import {initGame2, checkGame2} from '../js/game2';
+import * as Game2 from '../js/game2';
 
   //Elementos del DOM
     //JUEGO 1
@@ -8,7 +8,7 @@ import {initGame2, checkGame2} from '../js/game2';
     let resultTextAdivinador = $("#results-text-adivinador");  
     //JUEGO 2
     let gamePensador = $(".game-pensador");
-
+    let resultTextPensador = $("#results-text-pensador");
   
   //EVENTOS JUEGO 1
   //Inicia el juego
@@ -19,9 +19,8 @@ import {initGame2, checkGame2} from '../js/game2';
     //funcion para generar el numero 
     Game1.initGame1();
     //En caso de que haya un juego activo y se inicie otro
-    console.log(Game1.activeGame1)
     if(Game1.activeGame1 == true){
-      resultTextAdivinador.html("COMENZÓ OTRO JUEGO DE 'ADIVINA MI NUMERO'<br> ---------- <br>" + resultTextAdivinador.html());
+      resultTextAdivinador.html("COMENZÓ OTRO JUEGO DE 'ADIVINA MI NÚMERO'<br> ---------- <br>" + resultTextAdivinador.html());
     }
     Game1.activeGame1 = true
     //Muestro en consola numero ganador
@@ -35,24 +34,74 @@ import {initGame2, checkGame2} from '../js/game2';
     let numeroAnalizado = Game1.checkGame1(valorIngresadoAdivinador,Game1.numeroGame1);
     if(numeroAnalizado.valido == true){
       if(numeroAnalizado.correctos == 4){
-        resultTextAdivinador.html("<strong> GANASTE! EL NUMERO ERA: " + Game1.numeroGanador + " <br> INTENTOS: " + Game1.intentosGame1 + "<br> ---------- <br></strong>" + resultTextAdivinador.html())
+        resultTextAdivinador.html("<strong> GANASTE! EL NÚMERO ERA: " + Game1.numeroGanador + " <br> INTENTOS: " + Game1.intentosGame1 + "<br> ---------- <br></strong>" + resultTextAdivinador.html())
       }else{
-        resultTextAdivinador.html("Numero ingresado:" + numeroAnalizado.numero + "<br>" + numeroAnalizado.correctos + " BIEN Y " + numeroAnalizado.regulares + " REGULAR <br> ---------- <br>" + resultTextAdivinador.html())
+        resultTextAdivinador.html("Número ingresado:" + numeroAnalizado.numero + "<br>" + numeroAnalizado.correctos + " BIEN Y " + numeroAnalizado.regulares + " REGULAR <br> ---------- <br>" + resultTextAdivinador.html())
       }
     }else{
-      alert("Por favor ingresa un numero positivo de 4 cifras");
+      alert("Por favor ingresa un número positivo de 4 cifras");
     }
   });
 
 
   //EVENTOS JUEGO 2
+
+  //Inicia el juego
   $("#pensador").click(()=>{
     //se muestra html oculto correspondiente al segundo juego
     gameAdivinador.addClass("hide");
     gamePensador.removeClass("hide");
     //funcion para iniciar segundo juego, se genera un numero aleatorio de 4 cifras distintas
-    initGame2();
+    Game2.initGame2();
+    //En caso de que haya un juego activo y se inicie otro
+    if(Game2.activeGame2 == true){
+      resultTextPensador.html("COMENZÓ OTRO JUEGO DE 'ADIVINARÉ TU NÚMERO'<br> ---------- <br>" + resultTextPensador.html());
+    }
+    Game2.activeGame2 = true
+    //Muestro el candidato actual y lleno resultados
+    $("#answer-pensador").html(Game2.numeroCandidatoActual);
+    console.log("JUEGO 2 candidato: " + Game2.numeroCandidatoActual);
+    resultTextPensador.html("Posible candidato: " + Game2.numeroCandidatoActual + "<br>" + resultTextPensador.html())
   });
+
+
+  //Verifica las opciones ingresadas
   $("#send-pensador").click(()=>{
-    checkGame2()    
+    let respuestaBien = $("#bien-pensador").val();
+    let respuestaRegular = $("#regular-pensador").val();
+    let respuestaNumero = $("#answer-pensador").html();
+    let roundRespuestaBien = Math.round(respuestaBien);
+    let roundRespuestaRegular = Math.round(respuestaRegular);
+    let sumaRespuestas = roundRespuestaBien + roundRespuestaRegular
+    //Control sobre los campos de respuesta bien y regular
+    if (respuestaBien != "" && respuestaRegular != "" && roundRespuestaBien >= 0 && roundRespuestaBien <= 4 && roundRespuestaRegular >= 0 && roundRespuestaRegular <= 4 && sumaRespuestas >= 0 && sumaRespuestas <= 4) {
+      //vacio los inputs
+      $("#bien-pensador").val("")
+      $("#regular-pensador").val("");
+
+      if (respuestaBien == 4) {
+        resultTextPensador.html("<strong> TU NUMERO ES: " + respuestaNumero +"<br>INTENTOS: "+ Game2.intentosGame2 + "<br> ---------- <br></strong>" + resultTextPensador.html());
+      } else{
+        Game2.agregarACandidatosAnteriores(respuestaBien,respuestaRegular,respuestaNumero);
+        // Busco otro numero del arreglo
+        while (Game2.candidatoEncontrado == false) {
+          //En caso de error humano, el arreglo se vacia y muestro este mensaje
+          if (Game2.opcionesGame2.length == 0) {
+            resultTextPensador.html("No pude encontrar tu numero, por favor toca el boton de 'Adivinaré tu número nuevamente para iniciar otro juego! " + "<br>" + "----------<br>" + resultTextPensador.html());
+            return false;
+          } else {
+            let nuevoCandidato = Game2.opcionesGame2[Math.round(Math.random() * (Game2.opcionesGame2.length - 1))].toString();
+            // Compruebo que el candidato encontrado verifica los anteriores
+            Game2.validarCandidato(nuevoCandidato,Game2.numerosCandidatosGame2);
+          }
+        }
+        //Pregunto con nuevo candidato
+        resultTextPensador.html("Posible candidato: " + Game2.numeroCandidatoActual + "<br>" + "----------<br>" + "Bien: " + roundRespuestaBien + ", Regular: " + roundRespuestaRegular + "<br>" + resultTextPensador.html());
+        $("#answer-pensador").html(Game2.numeroCandidatoActual);
+        console.log("candidato: " + Game2.numeroCandidatoActual);
+      }
+    } else { 
+      alert("Por favor ingresa un numero de 0 a 4 en bien y regular.");
+    }
+    
   });
---
+++ test/game2.spec.js
@@ -0,0 +1,50 @@
+import * as Game2 from '../js/game2'
+
+describe('JUEGO 2: "Adivinaré mi número"', ()=> {
+  
+  describe('metodo initGame2', ()=> {
+    it('El posible candidato generado es de 4 digitos ',()=>{
+      Game2.initGame2();
+      expect(4).toEqual(Game2.numeroCandidatoActual.toString().length);
+    })
+  })
+  
+  describe('metodo agregarACandidatosAnteriores', ()=> {
+    it('Los numero es borrado del arreglo opcionesGame2 ',()=>{
+      Game2.initGame2();
+      let bien = 2;
+      let regular = 2;
+      let numero = 1234;
+      Game2.agregarACandidatosAnteriores(bien,regular,numero);
+      expect(Game2.opcionesGame2).not.toContain(numero);
+    })
+  })
+
+  describe('metodo validarCandidato', ()=> {
+    it('Verifica que el candidatoNuevo si cumple con las validaciones (Bien, Regular)',()=>{
+      Game2.initGame2();
+      let candidatoInicial = 1234
+      let candidatoNuevo = 1243
+      let bien = 2;
+      let regular = 2;
+      Game2.agregarACandidatosAnteriores(bien,regular,candidatoInicial);
+      Game2.validarCandidato(candidatoNuevo,Game2.numerosCandidatosGame2)
+      let candidatoFinal = Game2.numeroCandidatoActual
+      expect(candidatoNuevo).toEqual(candidatoFinal);
+    })
+  })
+})
+//   describe('metodo checkGame1', ()=> {
+//     let numeroGanador = ["1","2","3","4"];
+//     let numeroIngresado = "1234";
+//     let resultadoCheckGame1 = Game1.checkGame1(numeroIngresado,numeroGanador);
+  
+//       expect(4).toEqual(resultadoCheckGame1.correctos);
+//     })
+//     it('Los digitos regulares("REGULARES") son 0 ',()=>{
+//       expect(0).toEqual(resultadoCheckGame1.regulares);
+//     })
+//     it('La opcion ingresada tiene 4 digitos ',()=>{
+//       expect(true).toEqual(resultadoCheckGame1.valido);
+//     })
+//   })
